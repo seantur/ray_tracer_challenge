073d0854bd0e80e43186811cb0cd1f0cf39796fa
Sean Turner <seanturner@outlook.com>
2020-12-07 21:23:35 -0500

    removed PPM code in favor of png builtin (also 3x faster)

--- canvas/canvas.go
+++ canvas/canvas.go
@@ -1,98 +1,15 @@
 package canvas
 
 import (
-	"fmt"
 	"image"
 	"image/jpeg"
 	"image/png"
-	"io/ioutil"
 	"log"
 	"os"
-	"strconv"
-	"strings"
-)
-
-const (
-	PPMHeader  = "P3"
-	PPMCharLen = 70
-)
-
-const (
-	ErrOutOfBounds = "trying to access out of bounds"
 )
 
 func InitCanvas(height, width int) *image.RGBA64 {
 	return image.NewRGBA64(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{width, height}})
-}
-
-// WritePixel -> Set
-// At -> At
-
-func scale255(color, alpha uint32) string {
-	var val uint8
-
-	if alpha != 0 {
-		val = uint8(float64(color) / float64(alpha) * 255)
-	}
-
-	if val < 0 {
-		return "0 "
-	} else if val > 255 {
-		return "255 "
-	} else {
-		return strconv.Itoa(int(val)) + " "
-	}
-}
-
-func addColor(color, alpha uint32, s *strings.Builder, row *string) {
-	colorStr := scale255(color, alpha)
-
-	if len([]rune(*row))+len([]rune(colorStr)) > PPMCharLen {
-		writeRow(s, row)
-		*row = colorStr
-	} else {
-		*row += colorStr
-	}
-
-}
-
-func writeRow(s *strings.Builder, row *string) {
-	s.WriteString((*row)[:len((*row))-1] + "\n")
-}
-
-func toPPM(c image.Image) string {
-	var str strings.Builder
-
-	max := c.Bounds().Max
-	width, height := max.X, max.Y
-
-	str.WriteString(fmt.Sprintf("%s\n%d %d\n255\n", PPMHeader, width, height))
-
-	for i := 0; i < height; i++ {
-		row := ""
-		for j := 0; j < width; j++ {
-			color := c.At(j, i)
-
-			red, green, blue, alpha := color.RGBA()
-
-			addColor(red, alpha, &str, &row)
-			addColor(green, alpha, &str, &row)
-			addColor(blue, alpha, &str, &row)
-
-		}
-		writeRow(&str, &row)
-	}
-
-	return str.String()
-}
-
-func SavePPM(c image.Image, path string) {
-	err := ioutil.WriteFile(path, []byte(toPPM(c)), 0644)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
 }
 
 func SavePng(c image.Image, path string) {
--- canvas/canvas_test.go
+++ canvas/canvas_test.go
@@ -2,7 +2,6 @@ package canvas
 
 import (
 	"github.com/seantur/ray_tracer_challenge/raytracing"
-	"strings"
 	"testing"
 )
 
@@ -15,23 +14,6 @@ func TestCanvas(t *testing.T) {
 		if got != want {
 			t.Errorf("got %f want %f", got, want)
 		}
-	}
-
-	assertStringLine := func(t *testing.T, got string, want_line string, line int) {
-		t.Helper()
-
-		got_split := strings.Split(got, "\n")
-
-		if len(got_split) <= line {
-			t.Fatal("trying to compare line out of bounds")
-		}
-
-		got_line := got_split[line]
-
-		if got_line != want_line {
-			t.Errorf("got %s want %s", got_line, want_line)
-		}
-
 	}
 
 	t.Run("Create a canvas", func(t *testing.T) {
@@ -68,65 +50,4 @@ func TestCanvas(t *testing.T) {
 		raytracing.AssertColorsEqual(t, output, Red)
 	})
 
-	t.Run("construct PPM header", func(t *testing.T) {
-		c := InitCanvas(3, 5)
-
-		assertStringLine(t, toPPM(c), "P3", 0)
-		assertStringLine(t, toPPM(c), "5 3", 1)
-		assertStringLine(t, toPPM(c), "255", 2)
-
-	})
-
-	t.Run("construct PPM pixel data", func(t *testing.T) {
-		c := InitCanvas(3, 5)
-
-		c1 := raytracing.RGB{Red: 1.5, Green: 0, Blue: 0}
-		c2 := raytracing.RGB{Red: 0, Green: 0.5, Blue: 0}
-		c3 := raytracing.RGB{Red: -0.5, Green: 0, Blue: 1}
-
-		c.SetRGBA64(0, 0, c1.Cvt())
-		c.SetRGBA64(2, 1, c2.Cvt())
-		c.SetRGBA64(4, 2, c3.Cvt())
-
-		got := toPPM(c)
-
-		assertStringLine(t, got, "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0", 3)
-		assertStringLine(t, got, "0 0 0 0 0 0 0 127 0 0 0 0 0 0 0", 4)
-		assertStringLine(t, got, "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255", 5)
-
-	})
-
-	t.Run("PPM lines > 70 are on a new line", func(t *testing.T) {
-		c := InitCanvas(2, 10)
-
-		max := c.Bounds().Max
-		width, height := max.X, max.Y
-
-		color := raytracing.RGB{1, 0.8, 0.6}
-
-		for i := 0; i < width; i++ {
-			for j := 0; j < height; j++ {
-				c.Set(i, j, color)
-			}
-		}
-
-		got := toPPM(c)
-
-		assertStringLine(t, got, "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204", 3)
-		assertStringLine(t, got, "153 255 204 153 255 204 153 255 204 153 255 204 153", 4)
-		assertStringLine(t, got, "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204", 5)
-		assertStringLine(t, got, "153 255 204 153 255 204 153 255 204 153 255 204 153", 6)
-
-	})
-
-	t.Run("ensure ppm string ends with newline", func(t *testing.T) {
-		c := InitCanvas(5, 3)
-
-		ppm := toPPM(c)
-		endPPM := ppm[len(ppm)-1]
-
-		if endPPM != '\n' {
-			t.Error("did not end with newline")
-		}
-	})
 }
